19cd8f59a7898cdf1a5a9dc467472ac9add5ddf9
John Charles Woods <woods.johnny.charles@gmail.com>
2026-04-01 19:29:04 -0400

    Remove aframe dependency and add Three.js vector conversion methods
    - Remove aframe and all related dependencies from package.json and package-lock.json
    - Add Three.js import to vectors.ts for native Three.js vector conversions
    - Implement invertVector() and backwardsMove() methods for Vector2D, Vector3D, and Vector4D classes
    - Add toThreeJsVector2(), toThreeJsVector3(), and toThreeJsVector4() conversion methods
    - Create PlayerAttachedCamera class extending Three.js Camera
    - Mark is-buffer and xtend as dev dependencies

---
+++ src/browser/game/rendering/PlayerAttachedCamera.ts
@@ -0,0 +1,5 @@
+import { Camera } from "three";
+
+class PlayerAttachedCamera extends Camera {
+    
+}
--- src/common/vectors.ts
+++ src/common/vectors.ts
@@ -1,3 +1,4 @@
+import * as THREE from 'three';
 export class Vector2D {
     x: number;
     y: number;
@@ -8,6 +9,21 @@ export class Vector2D {
     move(vector: Vector2D){
         this.x += vector.x;
         this.y += vector.y;
+    }
+
+    invertVector(vector: Vector2D = this){
+        return new Vector2D(
+            -vector.x,
+            -vector.y
+        );
+    }
+
+    backwardsMove(vector: Vector2D){
+        this.move(this.invertVector(vector));
+    }
+
+    toThreeJsVector2(){
+        return new THREE.Vector2(this.x, this.y);
     }
 }
 
@@ -20,5 +36,47 @@ export class Vector3D extends Vector2D {
     move(vector: Vector3D){
         super.move(vector);
         this.z += vector.z;
+    }
+
+    invertVector(vector: Vector3D = this): Vector3D {
+        return new Vector3D(
+            -vector.x,
+            -vector.y,
+            -vector.z
+        );
+    }
+
+    backwardsMove(vector: Vector3D): void {
+        this.move(this.invertVector(vector));
+    }
+
+    toThreeJsVector3(){
+        return new THREE.Vector3(this.x,this.z,this.y);
+    }
+}
+
+export class Vector4D extends Vector3D {
+    w: number;
+    constructor(x: number, y: number, z: number, w: number){
+        super(x,y,z);
+        this.w=w;
+    }
+
+    move(vector: Vector4D) {
+        super.move(vector);
+        this.w+=vector.w;
+    }
+
+    invertVector(vector: Vector4D = this): Vector4D {
+        return new Vector4D(
+            -vector.x,
+            -vector.y,
+            -vector.z,
+            -vector.w
+        );
+    }
+
+    toThreeJsVector4(){
+        return new THREE.Vector4(this.x,this.y,this.z,this.w);
     }
 }
